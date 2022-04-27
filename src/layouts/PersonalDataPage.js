ad69cbd161c3ff84c47575874acaf35bd72d28e7
VilteBa <v.baltmiskyte@gmail.com>
2022-04-27 22:28:50 +0300

    show success alert on personal data update

--- src/layouts/PersonalDataPage.js
+++ src/layouts/PersonalDataPage.js
@@ -9,10 +9,12 @@ import {
   Label,
   Input,
   FormFeedback,
+  Alert,
 } from "reactstrap";
 import axios from "axios";
 
 const PersonalDataPage = () => {
+  const [alert, setAlert] = useState(false);
   const userData = JSON.parse(localStorage.getItem("user"));
   const [shelters, setShelters] = useState([]);
   const [errors, setErrors] = useState({});
@@ -39,8 +41,8 @@ const PersonalDataPage = () => {
     const isValid = validate(e);
     if (!isValid) return;
     console.log(inputs);
-    axios.patch(`Customer/UpdateUser`, inputs);
-    //todo: reik popup ar alert kad duomenys atnaujinti
+    axios.patch(`Customer/UpdateUser`, inputs).then(setAlert(true));
+    setErrors({});
   };
 
   const validate = (e) => {
@@ -63,94 +65,105 @@ const PersonalDataPage = () => {
   };
 
   return (
-    <Card>
-      <CardTitle tag="h4" className="border-bottom p-3 mb-0">
-        <i class="bi bi-person-lines-fill me-2"></i>
-        Mano Profilis
-      </CardTitle>
-      <CardBody>
-        <Form onSubmit={handleSubmit}>
-          <FormGroup>
-            <Label for="emailAddress">El. paštas</Label>
-            <Input
-              id="emailAddress"
-              type="emailAddress"
-              defaultValue={inputs.emailAddress}
-              onChange={handleChange}
-              invalid={errors["emailAddress"] === true}
-              valid={errors["emailAddress"] === false}
-            />
-            <FormFeedback>Neteisingas el. pašto formatas</FormFeedback>
-          </FormGroup>
-          <FormGroup>
-            <Label for="firstName">Vardas</Label>
-            <Input
-              id="firstName"
-              type="text"
-              defaultValue={inputs.firstName}
-              onChange={handleChange}
-              invalid={errors["firstName"] === true}
-              valid={errors["firstName"] === false}
-            />
-            <FormFeedback>Įveskite vardą</FormFeedback>
-          </FormGroup>
-          <FormGroup>
-            <Label for="lastName">Pavardė</Label>
-            <Input
-              id="lastName"
-              type="text"
-              defaultValue={inputs.lastName}
-              onChange={handleChange}
-              invalid={errors["lastName"] === true}
-              valid={errors["lastName"] === false}
-            />
-            <FormFeedback>Įveskite pavardę</FormFeedback>
-          </FormGroup>
-
-          <FormGroup>
-            <Label for="phoneNumber">Mobilusis numeris</Label>
-            <Input
-              id="phoneNumber"
-              type="text"
-              defaultValue={inputs.phoneNumber}
-              onChange={handleChange}
-              invalid={errors["phoneNumber"] === true}
-              valid={errors["phoneNumber"] === false}
-            />
-            <FormFeedback>
-              Įveskite telefono numerį (formatas +370********)
-            </FormFeedback>
-          </FormGroup>
-          {userData.role === "Worker" ? (
+    <>
+      <Alert
+        color="success"
+        isOpen={alert}
+        toggle={() => {
+          setAlert(false);
+        }}
+      >
+        <b>Informacija atnaujinta sėkmingai!</b>
+      </Alert>
+      <Card>
+        <CardTitle tag="h4" className="border-bottom p-3 mb-0">
+          <i class="bi bi-person-lines-fill me-2"></i>
+          Mano Profilis
+        </CardTitle>
+        <CardBody>
+          <Form onSubmit={handleSubmit}>
             <FormGroup>
-              <Label for="shelterId">Prieglauda</Label>
+              <Label for="emailAddress">El. paštas</Label>
               <Input
-                id="shelterId"
-                type="select"
+                id="emailAddress"
+                type="emailAddress"
+                defaultValue={inputs.emailAddress}
                 onChange={handleChange}
-                value={inputs.shelterId}
-                invalid={errors["shelterId"] === true}
-                valid={errors["shelterId"] === false}
-              >
-                <option />
-                {shelters.map((s, i) => (
-                  <option key={i} value={s.shelterId}>
-                    {s.name}
-                  </option>
-                ))}
-              </Input>
-              <FormFeedback>Pasirinkite prieglaudą</FormFeedback>
+                invalid={errors["emailAddress"] === true}
+                valid={errors["emailAddress"] === false}
+              />
+              <FormFeedback>Neteisingas el. pašto formatas</FormFeedback>
             </FormGroup>
-          ) : (
-            <></>
-          )}
+            <FormGroup>
+              <Label for="firstName">Vardas</Label>
+              <Input
+                id="firstName"
+                type="text"
+                defaultValue={inputs.firstName}
+                onChange={handleChange}
+                invalid={errors["firstName"] === true}
+                valid={errors["firstName"] === false}
+              />
+              <FormFeedback>Įveskite vardą</FormFeedback>
+            </FormGroup>
+            <FormGroup>
+              <Label for="lastName">Pavardė</Label>
+              <Input
+                id="lastName"
+                type="text"
+                defaultValue={inputs.lastName}
+                onChange={handleChange}
+                invalid={errors["lastName"] === true}
+                valid={errors["lastName"] === false}
+              />
+              <FormFeedback>Įveskite pavardę</FormFeedback>
+            </FormGroup>
 
-          <Button type="submit" size="lg" color="primary" block>
-            Atnaujinti duomenis
-          </Button>
-        </Form>
-      </CardBody>
-    </Card>
+            <FormGroup>
+              <Label for="phoneNumber">Mobilusis numeris</Label>
+              <Input
+                id="phoneNumber"
+                type="text"
+                defaultValue={inputs.phoneNumber}
+                onChange={handleChange}
+                invalid={errors["phoneNumber"] === true}
+                valid={errors["phoneNumber"] === false}
+              />
+              <FormFeedback>
+                Įveskite telefono numerį (formatas +370********)
+              </FormFeedback>
+            </FormGroup>
+            {userData.role === "Worker" ? (
+              <FormGroup>
+                <Label for="shelterId">Prieglauda</Label>
+                <Input
+                  id="shelterId"
+                  type="select"
+                  onChange={handleChange}
+                  value={inputs.shelterId}
+                  invalid={errors["shelterId"] === true}
+                  valid={errors["shelterId"] === false}
+                >
+                  <option />
+                  {shelters.map((s, i) => (
+                    <option key={i} value={s.shelterId}>
+                      {s.name}
+                    </option>
+                  ))}
+                </Input>
+                <FormFeedback>Pasirinkite prieglaudą</FormFeedback>
+              </FormGroup>
+            ) : (
+              <></>
+            )}
+
+            <Button type="submit" size="lg" color="primary" block>
+              Atnaujinti duomenis
+            </Button>
+          </Form>
+        </CardBody>
+      </Card>
+    </>
   );
 };
 
